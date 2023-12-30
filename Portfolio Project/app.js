53858876c6f5321f98b79670a59efef8f4760ef8
rizapeter <113911763+rizapeter@users.noreply.github.com>
2023-12-30 11:20:58 +0530

    Add files via upload

---
+++ Portfolio Project/app.js
@@ -0,0 +1,92 @@
+// toggle icon bar
+let menuIcon =  document.querySelector('#menu-icon');
+let navbar =  document.querySelector('.nav-bar');
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+// scroll
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll=() =>{
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset =  sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id= sec.getAttribute('id');
+
+        if(top >= offset && top <offset + heigth){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+
+        };
+    });
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+// remove toggle icon and navbar when clcik nabar link
+menuIcon.classList.remove('x');
+navbar.classList.remove('active');
+};
+
+// scroll
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .education-container, .container, .skills, .contact form', { origin: 'top' });
+// Toggle icon bar
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.nav-bar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx bx-x');
+//     navbar.classList.toggle('active');
+// };
+
+// // Scroll
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 link.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         }
+//     });
+
+//     // Sticky navbar
+//     let header = document.querySelector('header');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+
+//     // Remove toggle icon and navbar when clicking navbar link
+//     menuIcon.classList.remove('x');
+//     navbar.classList.remove('active');
+// };
+
+// // Scroll Reveal
+// ScrollReveal({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200
+// });
+
+// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .education-container, .container, .skills, .contact form', { origin: 'top' });
